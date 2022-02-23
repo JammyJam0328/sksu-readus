6ff7592d7c8954ec1b4e08be92c5076651f9bef6
norjamillekasan <88083841+norjamillekasan@users.noreply.github.com>
2022-02-23 10:32:04 +0800

    nginx 1

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,6 +36,8 @@ window.Echo = new Echo({
     key: "readUsAppKey",
     wsHost: window.location.hostname,
     wsPort: 6001,
+    wssHost: "127.0.0.1",
+    wssPort: 6001,
     disableStats: true,
     enabledTransports: ["ws", "wss"],
 });

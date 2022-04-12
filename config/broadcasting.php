4e9d0a90a2bcb22ee869821ee3c71f7275a672a0
norjamillekasan <88083841+norjamillekasan@users.noreply.github.com>
2022-04-12 10:10:54 +0800

    reason2

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -53,24 +53,24 @@ return [
 
 
         
-        // 'pusher' => [
-        //     'driver' => 'pusher',
-        //     'key' => env('PUSHER_APP_KEY'),
-        //     'secret' => env('PUSHER_APP_SECRET'),
-        //     'app_id' => env('PUSHER_APP_ID'),
-        //     'options' => [
-        //         'cluster' => env('PUSHER_APP_CLUSTER'),
-        //         'useTLS' => true,
-        //         'encrypted' => false,
-        //         'host' => '127.0.0.1',
-        //         'port' => 6001,
-        //         'scheme' => 'https',
-        //         'curl_options' => [
-        //             CURLOPT_SSL_VERIFYHOST => 0,
-        //             CURLOPT_SSL_VERIFYPEER => 0,
-        //         ]
-        //     ],
-        // ],
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
+                'encrypted' => false,
+                'host' => '127.0.0.1',
+                'port' => 6001,
+                'scheme' => 'https',
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ]
+            ],
+        ],
 
 
         'ably' => [
